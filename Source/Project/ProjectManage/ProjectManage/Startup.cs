a00d1914030a2fa85055d42b45d02f87e1505cdd
NguyenThiHao <nguyenhaomta@gmail.com>
2017-11-10 22:50:31 +0700

    upload source and database

---
+++ Source/Project/ProjectManage/ProjectManage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectManage.Startup))]
+namespace ProjectManage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
